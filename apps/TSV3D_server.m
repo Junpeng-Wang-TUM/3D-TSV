873b260d7de904a5beac11c0cb57a9ec89b10477
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2022-01-21 21:31:13 +0100

    Fixed typo.

--- apps/TSV3D_server.m
+++ apps/TSV3D_server.m
@@ -24,7 +24,7 @@
 % OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 % OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-function TSV3D_worker
+function TSV3D_server
 	% Either call the function javaclasspath below, or use:
     % cd(prefdir)
     % edit javaclasspath.txt
